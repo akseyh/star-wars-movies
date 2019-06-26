aa651791c877698416056bf317160315c7570501
Şems Yılmaz <akgulmuhammed@live.com>
2019-06-26 21:40:30 +0300

    openCardHeader function added

--- app.js
+++ app.js
@@ -4,25 +4,28 @@ window.onload = function getJSON (){
     }).then(function(movies){
         movies.results.forEach(element => {
             let createCard = document.createElement("div");
-            createCard.setAttribute('class','card mb-4');
+            createCard.setAttribute('class', 'card mb-1');
+            createCard.setAttribute('onclick', 'openCardHeader(this)');
+
             let createCardHeader = document.createElement("div");
-            createCardHeader.setAttribute('class','card-header d-flex justify-content-between');
+            createCardHeader.setAttribute('class', 'card-header d-flex justify-content-between');
+
             let createH4 = document.createElement("H4");
             createH4.innerHTML = element.title;
+
             createCardHeader.appendChild(createH4);
             createCard.appendChild(createCardHeader);
             document.getElementById("app").appendChild(createCard);
 
             let createCardBody = document.createElement("div");
-            createCardBody.setAttribute('visibility','hidden');
-            createCardBody.setAttribute('class','card-body');
+            createCardBody.style.display = 'none';
+            createCardBody.setAttribute('class', 'card-body');
             createCard.appendChild(createCardBody);
 
             element.characters.forEach(characters => {
                 fetch(characters).then(function(response){
                     return response.json();
                 }).then(function(character){
-                    console.log(character);
                     let cardText = document.createElement("H6");
                     cardText.innerHTML = character.name;
                     createCardBody.appendChild(cardText);
@@ -30,4 +33,12 @@ window.onload = function getJSON (){
             });
         });
     })
-} 
+}
+
+function openCardHeader(myDiv) {
+    isOpen = myDiv.childNodes[1].style.display != 'none' ? true : false;
+
+    if(isOpen) {
+        myDiv.childNodes[1].style.display = 'none';
+    } else myDiv.childNodes[1].style.display = 'block';
+}
