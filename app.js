72ebbc432bb41f283f0bd4b1b695b6d7b3065157
Şems Yılmaz <akgulmuhammed@live.com>
2019-06-26 20:11:29 +0300

    added api

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+window.onload = function getJSON (){
+    fetch("https://swapi.co/api/films/?format=json").then(function(response){
+        return response.json();
+    }).then(function(movies){
+        movies.results.forEach(element => {
+            let createCard = document.createElement("div");
+            createCard.setAttribute('class','card mb-4');
+            let createCardHeader = document.createElement("div");
+            createCardHeader.setAttribute('class','card-header d-flex justify-content-between');
+            let createH4 = document.createElement("H4");
+            createH4.innerHTML = element.title;
+            createCardHeader.appendChild(createH4);
+            createCard.appendChild(createCardHeader);
+            document.getElementById("app").appendChild(createCard);
+
+            let createCardBody = document.createElement("div");
+            createCardBody.setAttribute('visibility','hidden');
+            createCardBody.setAttribute('class','card-body');
+            createCard.appendChild(createCardBody);
+
+            element.characters.forEach(characters => {
+                fetch(characters).then(function(response){
+                    return response.json();
+                }).then(function(character){
+                    console.log(character);
+                    let cardText = document.createElement("H6");
+                    cardText.innerHTML = character.name;
+                    createCardBody.appendChild(cardText);
+                })
+            });
+        });
+    })
+} 
