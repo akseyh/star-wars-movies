48bbd31aaac909a098791c1a98dea64f5fffc69d
Şems Yılmaz <akgulmuhammed@live.com>
2019-07-17 18:21:23 +0300

    Updates

---
+++ precache-manifest.ea94f6c32083d98c2032dd5bd170ade5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aeef5c68cb043082415c6ae6b4c2f5",
+    "url": "/akseyh/star-wars-movies/index.html"
+  },
+  {
+    "revision": "5f4875308debd62019da",
+    "url": "/akseyh/star-wars-movies/static/js/2.fddd2539.chunk.js"
+  },
+  {
+    "revision": "cc8eeb14bff4d93a84bc",
+    "url": "/akseyh/star-wars-movies/static/js/main.537b3533.chunk.js"
+  },
+  {
+    "revision": "a42e72fd793e579e8abf",
+    "url": "/akseyh/star-wars-movies/static/js/runtime~main.ade8c295.js"
+  },
+  {
+    "revision": "1b4306c004a3dd5080bdbcbbc0127b4a",
+    "url": "/akseyh/star-wars-movies/static/media/star-wars-logo.1b4306c0.png"
+  }
+]);
